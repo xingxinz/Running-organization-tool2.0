002ddfc9fefb2529e0a870e36ece4f4f9d8d45a5
yankai <kaiyanchn@foxmail.com>
2016-08-13 11:13:19 +0800

    rank localtest

--- Application/Home/Controller/RankController.class.php
+++ Application/Home/Controller/RankController.class.php
@@ -10,6 +10,13 @@ class RankController extends Controller {
             'appsecret'=>'630aad385d1eaa2eef012777ce8f9670' //填写高级调用功能的密钥
         );
 
+	public function _initialize(){
+		if ($_SERVER['SERVER_NAME'] == 'localhost'){
+			session('wechat_openid','123456');
+			session('user_id','999');
+		}
+	}	
+
 	public function signRank(){
         $API=A('API');
         if(!isset($_SESSION['wechat_openid'])){
@@ -40,4 +47,37 @@ class RankController extends Controller {
 
 		$this->display();
 	}
+	
+	public function dailyRank(){
+        $API=A('API');
+		
+        if(!isset($_SESSION['wechat_openid'])){
+            if(!isset($_SESSION['user_id'])){
+                $url = 'http://'.$_SERVER['SERVER_NAME'].'/index.php/Home/Rank/dailyRank';
+                $API->getOauth($url);
+                die();
+            }else{
+                echo "<h1>尚未绑定微信！</h1>";
+                die();
+            }
+        }
+            
+		$Rank=M('User');
+		$list=$Rank->where('days > 0')->order('point desc,days desc')->select();
+		$myRecord=$Rank->where('id='.session('user_id'))->find();
+
+		foreach($list as $key => $val){ //确定用户排名
+			if($val['id'] == session('user_id')){
+				$myRecord['rank']=$key+1;
+			}
+		}
+		if($myRecord['rank']==null){
+			$myRecord['rank']=-1;
+		}
+		$this->assign('myRecord',$myRecord);
+		$this->assign('list',$list);
+
+		$this->display();
+	}
+
 }
